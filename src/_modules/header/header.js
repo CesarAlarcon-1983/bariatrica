766951fdc4a415851d358ba0fbc1a062b3df1f85
Your Name <you@example.com>
2019-10-30 21:19:33 -0300

    Updates

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -25,6 +25,68 @@ var Header = function() {
             moreButton.html('ver más');
         }
     })
+
+    //switcher
+    var targets = $('[data-target]');
+    var contents = $('[data-content]');
+    var container = $('.por-donde-empiezo__options__container');
+
+    function switcherInit() {
+        $(targets[0]).addClass('-active');
+        $(contents[0]).addClass('-active');
+        container.height($(contents[0]).height());  
+    }
+
+    targets.on('click', function() {
+        targets.removeClass('-active');
+        contents.removeClass('-active');
+
+        var targettedContent = $(this).data('target');
+        var content = contents.filter('[data-content=' +  targettedContent + ']');
+
+        $(this).addClass('-active');
+        content.addClass('-active');
+
+        var wrapperHeight = content.height();
+
+        container.height(wrapperHeight);
+    })
+
+    switcherInit();
+
+    //faq
+
+    var questions = $('.faq__question');
+    var answers = $('.faq__answer');
+
+    questions.on('click', function() {
+        if(questions.length === answers.length) {
+
+            var questionIndex = questions.index($(this));
+
+            $(this).toggleClass('-open');
+            $(answers[questionIndex]).toggleClass('-open');
+
+        }
+    })
+
+    //calculo IMC
+    var cacularButton = $('.por-donde-empiezo__options__button');
+    var resultadoImc = $('.por-donde-empiezo__options__resultado-imc');
+
+    cacularButton.on('click', function() {
+        var estatura = $('.-estatura').val();
+        var peso = $('.-peso').val();
+        
+        if(!estatura || !peso) {
+            return
+        }
+        
+        var imc = (peso / (estatura/100)).toFixed(2);
+
+        resultadoImc.html(imc)
+                
+    })
 };
 
 module.exports = Header;
